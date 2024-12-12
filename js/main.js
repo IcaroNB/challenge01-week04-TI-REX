0162bc65f6839d5d594b5ba7c1615f3484cdc8ec
Ícaro Bezerra <icaronb14@gmail.com>
2024-12-12 14:00:47 -0400

    doing javascript initial actions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+// Capturing the HTML Elements and their values
+
+let subscribeEmail = document.getElementById("subscribeEmail").value;
+let subscribenamne = document.getElementById("subscribeEmail").value;
+
+const validateEmail = (email) => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; //Regex for email validation
+  const nameRegex = /^[a-zA-ZÀ-ÿ\s]+$/; //Regex for name validation
+};
