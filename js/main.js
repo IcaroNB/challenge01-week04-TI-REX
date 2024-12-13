28b3b050ab2914a89bbb867f56d9f1a0181a213d
Ícaro Bezerra <icaronb14@gmail.com>
2024-12-13 11:02:58 -0400

    replaced all the delete images to buttons and finishing the js part of home page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,51 @@
 // Capturing the HTML Elements and their values
 
-let subscribeEmail = document.getElementById("subscribeEmail").value;
-let subscribenamne = document.getElementById("subscribeEmail").value;
+const subscribeForm = document.getElementById("subscribeForm");
+const subscribeEmail = document.getElementById("subscribeEmail");
+const subscribeName = document.getElementById("subscribeName");
+const messageBox = document.getElementById("message");
 
-const validateEmail = (email) => {
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; //Regex for email validation
-  const nameRegex = /^[a-zA-ZÀ-ÿ\s]+$/; //Regex for name validation
+// Capturing events
+
+subscribeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const bruteEmail = subscribeEmail.value;
+  const email = bruteEmail.trim();
+
+  const bruteName = subscribeName.value;
+  const name = bruteName.trim();
+
+  messageBox.classList = "hidden";
+  messageBox.innerText = "";
+
+  const errorMessage = validateValues(email, name);
+
+  if (errorMessage) {
+    messageBox.classList.remove("hidden");
+    messageBox.classList.add("error");
+    messageBox.innerText = errorMessage;
+  } else {
+    messageBox.classList.remove("error");
+    messageBox.classList.add("hidden");
+    window.location.href = "/pages/login.html";
+  }
+});
+
+// Validating received values
+
+const validateValues = (email, name) => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const nameRegex = /^[a-zA-ZÀ-ÿ\s]+$/;
+
+  if (!email || !emailRegex.test(email)) {
+    return (messageBox.innerText = "Please, enter a valid e-mail.");
+  }
+
+  if (!name || !nameRegex.test(name)) {
+    return (messageBox.innerText =
+      "Please, inform a valid name. Must contain only letters and spaces.");
+  }
+
+  return null;
 };
